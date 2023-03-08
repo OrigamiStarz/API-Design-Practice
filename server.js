0b65dae334148252bb473dd8254e5406e0282402
OrigamiStarz <isabelbku@gmail.com>
2023-03-08 04:49:11 +0000

    Added comments to server.js.

---
+++ server.js
@@ -0,0 +1,25 @@
+// EXERCISE 1 // 
+
+// REQUIRE EXPRESS
+
+// INSTANTIATE EXPRESS
+
+// SERVE UP STATIC FILES IN PUBLIC FOLDER 
+
+// REQUIRE BODY-PARSER (EXERCISE 2) 
+ 
+// LISTEN ON PORT 8080
+
+// ENDPOINT 1: index.html
+
+// ENDPOINT 2: practice1.html
+
+// ENDPOINT 3: practice2.html
+
+// ENDPOINT 4: practice3.html
+
+// ENDPOINT 5: index.html 
+
+// EXERCISE 2 // 
+
+// HANDLE POST REQUEST
